272de10b49f365d1b6c18ac3871323f25e80b893
William L <w-lindvall@outlook.com>
2020-05-15 01:34:15 -0700

    Test `skip-to-list` tab navigation and focus

--- tests/e2e/integration/skip-to-list.js
+++ tests/e2e/integration/skip-to-list.js
@@ -1,4 +1,4 @@
-describe('skip-link list', () => {
+describe('Skip link list', () => {
   beforeEach(() => {
     cy.visit('/skip-to-list.html')
   })
@@ -7,5 +7,15 @@ describe('skip-link list', () => {
     cy
       .get('[data-vst="skip-to-list"]')
       .should('contain', 'Skip links')
+  })
+
+  it('Should traverse links on tab', () => {
+    cy.get('body').tab().tab()
+    cy.focused().should('contain', 'Footer')
+  })
+
+  it('Should focus relevant element on link press', () => {
+    cy.get('body').tab().click()
+    cy.focused().should('have.id', 'main')
   })
 })
