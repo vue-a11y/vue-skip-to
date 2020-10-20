ac33193fece9d35524c5f786a3bf25e29ebaebee
Alan Ktquez <ktquez@gmail.com>
2020-10-20 06:02:24 -0300

    build: Update config

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,19 +1,10 @@
 import buble from '@rollup/plugin-buble'
-import resolve from '@rollup/plugin-node-resolve'
-import chokidar from 'chokidar'
 import { eslint } from 'rollup-plugin-eslint'
-import livereload from 'rollup-plugin-livereload'
-import serve from 'rollup-plugin-serve'
 import vue from 'rollup-plugin-vue'
 
 export default {
   input: 'src/index.js',
-  watch: {
-    chokidar,
-    include: ['src/**']
-  },
   plugins: [
-    resolve(),
     eslint({
       include: './src/**'
     }),
@@ -21,22 +12,13 @@ export default {
       css: true,
       compileTemplate: true
     }),
-    buble(),
-    serve({
-      port: 5000,
-      verbose: true,
-      contentBase: 'demo',
-      historyApiFallback: true
-    }),
-    livereload({
-      watch: 'demo'
-    })
+    buble()
   ],
   output: [
     {
       name: 'VueSkipTo',
-      file: 'demo/vue-skip-to.js',
-      format: 'umd',
+      file: 'demo-cli/vue-skip-to.js',
+      format: 'esm',
       exports: 'named'
     }
   ]
